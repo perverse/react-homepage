c4563d7e48a35e6ed3fa2a5880379392093f4dc1
perverse <lol@lol.com>
2025-08-09 19:49:36 +1000

    Ported to vite instead of using nextjs

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,11 @@
-const nextJest = require('next/jest')
-
-const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
-})
-
-// Add any custom config to be passed to Jest
-const customJestConfig = {
+export default {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/test/setup.ts'],
-  testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   },
+  transform: {
+    '^.+\\.(t|j)sx?$': 'babel-jest'
+  }
 }
-
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Terminal from './components/sections/Terminal'
+
+function App() {
+  return (
+    <main className="relative">
+      <Terminal />
+    </main>
+  )
+}
+
+export default App
